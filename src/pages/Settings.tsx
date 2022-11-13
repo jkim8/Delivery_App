e3942f470e92e7c67f29df806fea3ba5e349f03e
jkim8 <juno0218@naver.com>
2022-11-12 19:45:08 -0500

    navigation

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,12 @@
+import {Button, Text, View} from 'react-native';
+
+function Settings() {
+  return (
+    <View>
+      <Text>Settings</Text>
+      <Button title="gotoDetail" />
+    </View>
+  );
+}
+
+export default Settings;
